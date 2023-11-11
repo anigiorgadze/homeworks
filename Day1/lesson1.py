085148228b1d72a9b794578aa4192044da69f623
anigiorgadze <anuukigiorgadze@gmail.com>
2023-11-11 22:34:27 +0400

    first commit

---
+++ Day1/lesson1.py
@@ -0,0 +1,11 @@
+name ="Ana"
+
+surname = "Giorgadze"
+age = 27
+height = 154.5
+weight = 48
+
+
+
+
+print("მე ვარ" + " " + name + " "  + surname + " "+ "ასაკია"+ " "+ str(age) + " " +"სიმაღლე"+ "-"+str(height)+ " " + "წონა" + "-" + str(weight))

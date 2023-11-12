3c246c548a8ab0b46ae723e4e9f3f1b060be4f4a
anigiorgadze <anuukigiorgadze@gmail.com>
2023-11-12 21:43:43 +0400

    day2 commit

---
+++ Day2/Casswork .py
@@ -0,0 +1,87 @@
+#4 ჯგუფი
+
+
+# print (7+10)
+# print("7"+"10")
+
+#print(40%3) ნაშთი გამოაქვს
+#print(10//2) ჭრის ციფრებს წერტილის შემდეგ
+
+
+# Dad_age = 50
+# Mom__age= 46
+# Sista_1__age = 22
+# Sista_2__age = 30
+# Bro_age = 11
+
+# Dad_age += 5
+# Mom__age += 5
+# Sista_1__age += 5
+# Sista_2__age += 5
+# Bro_age += 5
+
+
+
+#102 ჯგუფი
+
+# name = "ana" 
+
+# print(len(name))  #len length
+
+# my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას, 
+# იწყებთ 19 სექტემბერს, 
+# გთხოვთ არ გააცდინოთ"""  # სამი ბრჭყალით ვბეჭდავთ  ვრცელ ტექსტებს
+
+
+
+# IF 
+#knows_programming =True
+
+# if 10>5:
+#     print("hello")
+#     #tab-ის საშუალებით მოვახდინეთ იდენტაცია (indetation)
+
+# full_name = "ana_giorgadze"
+
+# print(len(full_name))
+# print(full_name[0])
+
+#yყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+#position = index სინონიმებია, 0დან იწყება თვლა
+
+# print ("z" in full_name)
+
+# print ("z" not in full_name)
+
+# print(full_name[2:7]) #მეორედან მეშვიდე სიმბოლომდე წამოიღებს სიტყვას
+# print(full_name[2:9:2]) #2დან 9მდე ყოველი მეორე 
+#start:finish:step
+
+# print(full_name[:3]) #დასაწყისიდან მესამემდე
+
+# print(full_name[4:]) #მეოთხედან ბოლომდე
+
+# print(full_name[-1]) #ბოლო სიმბოლო
+# print(full_name[-4]+"abc") 
+
+
+#print(full_name[-1:-6:-1]) # გვიბრუნებს სიტყვას
+
+#სტრინგებს აქვთ მეთოდები
+# 1) სტრინგ.upper() (lower)
+# 2) len (სტრინგი)
+
+# print(full_name.upper())
+
+#   .strip ზედმეტ სფეისებს აქრობს
+
+#repalce
+
+name2 = "ANA GIORGADZE"
+print(name2.replace("A","#"))
+
+
+
+
+
+
---
+++ Day2/homework.py
@@ -0,0 +1,11 @@
+full_name ="ana giorgadze"
+# print(len(full_name))
+#print("k" in full_name)
+# print(full_name[:3])
+# print(full_name[4:])
+# print(full_name[1:11:3])
+# print(full_name[-1::-1])
+# print(full_name[:3]+" "+"GIORGADZE")
+# print(full_name.replace("a","$"))
+
+

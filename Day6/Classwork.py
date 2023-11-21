4abf26ea1219c545769c3baa7875a0d4fe77e46a
anigiorgadze <anuukigiorgadze@gmail.com>
2023-11-21 22:06:17 +0400

    day6 commit

---
+++ Day6/Classwork.py
@@ -0,0 +1,106 @@
+#while [i] in "aeiou"
+#ინფუთითშეიტანეთ თქვენიი სახელი და გვარი;
+#ტერმინალში გამოიტანეთ რომელ ინდექსზე არის ხმოვნები
+
+# i=0
+# full_name = input("Enter your name: ")
+# while i < len(full_name):
+#     if full_name[i] in "aeiou":
+#         print(str(i)+full_name[i])
+#     i += 1
+
+
+
+# for i in range (4):
+#     for j in range(4):
+#         print("nika")
+
+
+# x=0
+# for i in range(5):   #0,1,2,3,4,
+#     for j in range(4):  #0,1,2,3
+#         for z in range(3):
+#             print(str(x)+" nika")
+#             x +=1
+
+
+        #მაგალითი
+# ხუთჯერ შემოდის ინფუთით ასაკი
+#თუ სამწელზე მეტისაა ახდევინებენ ფულს თითოს 100ლ
+
+
+# total_price=0
+# for i in range(5):
+#     age_of_user = int(input("enter users age: "))
+#     if age_of_user >=3:
+#         total_price +=100
+
+# print(total_price)
+
+#while-ით
+# total_price =0
+# i=0
+# while i < 5:
+#     age_of_user = int(input("enter users age: "))
+#     if age_of_user >=3:
+#         total_price +=100
+#     i +=1
+
+# print(total_price)
+
+
+
+    #მაგალითი sololearn-დან
+# n = int(input())
+
+# for x in range (1, n):
+#     if x % 2 == 1:
+#         if x % 3 == 0 and x % 5 == 0:
+#             print("Sololearn")
+#         elif x % 3 == 0:
+#             print("Solo")
+#         elif x % 5 == 0:
+#             print("Learn")
+#         else:
+#             print(x)
+
+
+#BMI Calculator (sololearn-დან მაგალითი)
+
+# user_weight = int(input("enter user's weight: "))
+# user_height = float(input("enter user's height: "))
+# bmi = user_weight/(user_height **2)
+
+# if bmi < 18.5:
+#     print("Underweight")
+# elif bmi >18.5 and bmi <25:
+#     print("Normal")
+# elif bmi >=25 and bmi <30:
+#     print("Overweight")
+# else:
+#     print("Obesity")
+
+
+
+#list = სია
+#int,float, boolean,list
+
+my_list = ["xinkali", "mwvadi", "lobiani", "qababi","khachapuri", "wyali"]
+ 
+prices =[2, 20, 15, 10, 15, 2]
+
+weird_list =["nika", "nika2", "nika3", [1,2,3], "nika4", [4,5,6]]
+
+
+
+# print(weird_list[3][1])
+
+
+
+#davaleba
+#xinkali girs 2 lari
+#mwvadi girs 20 lari
+
+
+
+
---
+++ Day6/Homework.py
@@ -0,0 +1,56 @@
+#დავალება 102 ჯგუფი
+
+# my_list = ["xinkali", "mwvadi", "lobiani", "qababi","khachapuri", "wyali"]
+ 
+# prices =[2, 20, 15, 10, 15, 2]
+
+# #xinkali girs 2 lari
+# #mwvadi girs 20 lari
+
+# i=0
+# for food in my_list:
+#     print("{} girs {} lari.".format(food, prices[i]))
+#     i+=1
+
+
+
+#დავალება 4 ჯგუფი
+#მომხმარებლის ნიშნებისგან გამოიანგარიშება საშუალო არითმეტიკული და თუ ცხრაზე მეტი იქნება
+#დაუპრინტეთ გილოცავ მატრიცელო შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რისთვისაც შეალიე შენი ცხოვრების წლები
+#თუ ეს იქნება 5ზე ნაკლები დაუპრინტე გაექეცი მატრიცას
+#თუ ეს იქნება 5დან 9მდე დაუპრინტე YOU ARE MID
+#თუ იქნება 10-ზე მეტი ან 0-ზე ნაკლები დაუპრინტე there is something wrong with you
+
+# score1= int(input("enter score 1: "))
+# score2 =int(input("enter score 2: "))
+# score3 =int(input("enter score 3: "))
+# score4 =int(input("enter score 4: "))
+# score5 =int(input("enter score 5: "))
+
+# average_score = (score1+score2+score3+score4+score5)/5
+
+# if average_score <0 or average_score>10:
+#     print("there is something wrong with you")
+# elif average_score > 9:
+#     print("გილოცავ, მატრიცელო! შენ გადმოგეცა 300 ლარიანი ბანძი ტოსტერი რისთვისაც შეალიე შენი ცხოვრების წლები")
+# elif average_score >= 5 and average_score<= 9:
+#     print("YOU ARE MID")
+# elif average_score <5:
+#     print ("გაექეცი მატრიცას")
+
+
+    #davaleba 2
+    #მომხმარებელს შეეკიტხეთ ხელფასი
+    #თუ 10000 მეტია დაუპრინტეთ გოა-ში სწავლობდი?
+    #1000-10000-ია, დაუპრინტეთ you mid
+    #თუ 1000-ზე დაბალია , დაუპრინტეთ, შემოსულიყავი გოაში მატრიცელო
+
+# salary = int(input("Enter your salary: "))
+# if salary > 10000:
+#     print("გოა-ში სწავლობდი?")
+# elif salary >= 1000 and salary<=10000:
+#     print("you mid")
+# elif salary < 1000:
+#     print("შემოსულიყავი გოაში მატრიცელო")
+
+
